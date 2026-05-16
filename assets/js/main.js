fa7bdc2bae7fb43d686f789162bb5d8a283249b9
Prasanna-7666 <prasannaone27@gmail.com>
2026-05-16 14:37:15 +0530

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,4 +151,25 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
+
+    // Back to Top Button
+    const backToTopBtn = document.createElement('div');
+    backToTopBtn.id = 'back-to-top';
+    backToTopBtn.innerHTML = '<i class="fa-solid fa-arrow-up"></i>';
+    document.body.appendChild(backToTopBtn);
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            backToTopBtn.classList.add('show');
+        } else {
+            backToTopBtn.classList.remove('show');
+        }
+    });
+
+    backToTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
 });
